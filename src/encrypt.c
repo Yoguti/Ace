f4277fa7d186664fffaee0a2b244e4450cf441b4
Yoguti <klaus2027@gmail.com>
2025-01-25 23:06:31 -0300

    update

--- src/encrypt.c
+++ src/encrypt.c
@@ -4,6 +4,14 @@
 #include "../include/definitions.h"
 
 char* caesar_encrypt(const char* path) {
+    FILE *file = fopen(path, "rb+");
+    if (file == NULL) {
+        perror("Unable to open file");
+        return STATUS_ERROR;
+    }
 
+    
+
+    fclose(file);
     return STATUS_SUCCESS;
 }
